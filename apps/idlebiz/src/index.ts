f2c3f2b4f7ffcb42e1313b28e00eba1b90298bf3
nsylke <nick@trpkit.com>
2025-04-25 22:40:08 -0500

    feat: create idlebiz app

---
+++ apps/idlebiz/src/index.ts
@@ -0,0 +1,15 @@
+import { Client } from "discord.js";
+
+const client = new Client({ intents: [] });
+
+client.once("ready", (readyClient) => {
+  console.log(`Logged in as ${readyClient.user.tag}!`);
+});
+
+(async () => {
+  try {
+    await client.login(process.env.DISCORD_TOKEN);
+  } catch {
+    await client.destroy();
+  }
+})();
